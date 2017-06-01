3f81b4604aa6ce17dfdf132c870fe5ac6cda15d0
Cheerleadernow <jocox@ucsc.edu>
2017-06-01 00:25:12 -0700

    new commit
    please add

--- Probability.py
+++ Probability.py
@@ -1,20 +1,19 @@
 # ------------------------------------------------------------------------------
 # Probability.py
-# Programming Assignment 5
+# Programming Assignment 6
 # Computer attempts to guess a number you choose between given integers,
 # ------------------------------------------------------------------------------
 import random
 frequency = []
 numberOfTrials = []
-
 def throwDice(m, k):
     """
    throws two independent dice and returns the result in a 2-tuple
     """
-    rolls = [0] * m
+    throws = []
     for r in range(0, m + 1):
-        rolls[r] = rng.randrange(1, k + 1)
-    return rolls
+        throws.append(rng.randrange(1, k + 1))
+    return throws
     # end throwDice()
 
     # -- main ---------------------------------------------------------------
@@ -23,27 +22,28 @@ print('\n')
 
 rollAgain = "Yes" or "yes" or "y"
 
-while rollAgain == "Yes" or "yes" or "y":
-
+while rollAgain:
     m = input('Enter the number of dice: ')  # input
     k = input('Enter the number of sides on each die: ')
     numberOfTrials = input('Enter the number of trials to perform: ')
 
     if m < 1:
-        m = input('Enter the number of dice: ').m()
+        m = input('Enter the number of dice: ')
 
-    if k < 2:
-        k = input("Enter the number of sides on each die: , ").k()
+    if k <= 2:
+        k = input("Enter the number of sides on each die: ")
 
-    if numberOfTrials > 1:
-        numberOfTrials = input("Enter the number of trials to perform: , ")
+    if numberOfTrials < 1:
+        numberOfTrials = input("Enter the number of trials to perform: ")
 
     rng = random.Random()  # Create a random number generator
 
-    while (m > 1) and (k > 2) and (numberOfTrials > 1):
-        print("Caclulating...")
-        print("------------------------------------------------------------------------------")
-        print(throwDice(m, k))
+    print("Caclulating...")
+    print("------------------------------------------------------------------------------")
+
+    rolls = throwDice(m, k)
+    for i in rolls:
+        print(i)
 
     for i in range(numberOfTrials):
         t = throwDice(m, k)
@@ -53,30 +53,43 @@ while rollAgain == "Yes" or "yes" or "y":
         print()
         print("Frequencies:")
         print(frequency)
+        break
+    relativeFrequency = [0, 0]
+    probability = [0, 0]
+    error = [0, 0]
+    for i in range (2, len (frequency)):
+        relativeFrequency.append (frequency[i] / numberOfTrials)
+        probability.append (min (i - 1, 13 - i) / 36)
+        error.append (abs (probability[i] - relativeFrequency[i]))
+        # end for
 # perform simulation, record and print frequencies
 # calculate relative frequencies, probabilities and errors
-relativeFrequency = [0, 0]
-probability = [0, 0]
-error = [0, 0]
-for i in range(2, len(frequency)):
-    relativeFrequency.append(frequency[i] / numberOfTrials)
-    probability.append(min(i - 1, 13 - i) / 36)
-    error.append(abs(probability[i] - relativeFrequency[i]))
-    # end for
 
 
-# print(relativeFrequency)
-# print(probability)
-# print(error)
+
+print(relativeFrequency)
+print(probability)
+print(error)
 print()
-
 # print results
+
 f1 = "{0:<10}{1:<22}{2:<22}{3:<22}"
 f2 = 71 * "-"
 f3 = "{0:>3}       {1:<22.15f}{2:<22.15f}{3:<.15f}"
+
 print(f1.format("Sum", "Relative Frequency", "Probability", "Error"))
 print(f2)
+
 for i in range(2, len(frequency)):
+    relativeFrequency = [0, 0]
+    probability = [0, 0]
+    error = [0, 0]
     print(f3.format(i, relativeFrequency[i], probability[i], error[i]))
-# end for
-print()
+        # end for
+
+    print [()]
+
+while rollAgain:
+    if no:
+        break
+
